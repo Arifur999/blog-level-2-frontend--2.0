3fda7208f006332657389f29f6d5f65d85d5e971
Arifur999 <arr96777@gmail.com>
2026-01-22 22:20:30 +0600

    fixed register form error

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -13,6 +13,20 @@ const eslintConfig = defineConfig([
     "build/**",
     "next-env.d.ts",
   ]),
+  {
+    rules :{
+      
+
+"react/no-children-prop": [
+  true,
+   {
+  allowFunctions: true,
+    }
+  ],
+
+},
+    }
+
 ]);
 
 export default eslintConfig;
