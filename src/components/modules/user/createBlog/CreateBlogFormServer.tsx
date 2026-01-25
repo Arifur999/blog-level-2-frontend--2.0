11542954fbe6fab6c283e28c89e95021fb820ce3
Arifur999 <arr96777@gmail.com>
2026-01-25 20:21:33 +0600

    add blog function

--- src/components/modules/user/createBlog/CreateBlogFormServer.tsx
+++ src/components/modules/user/createBlog/CreateBlogFormServer.tsx
@@ -1,7 +1,53 @@
 import { Button } from "@/components/ui/button";
 import { Card, CardContent, CardDescription, CardFooter, CardHeader, CardTitle } from "@/components/ui/card";
+import { Field, FieldGroup, FieldLabel } from "@/components/ui/field";
+import { Input } from "@/components/ui/input";
+import { env } from "@/env";
+import { cookies } from "next/headers";
+import { redirect } from "next/navigation";
 
+
+
+const API_URL=env.API_URL
 export default function CreateBlogFormServer() {
+
+const createBlog = async(formData : FormData)=>{
+"use server";
+
+const title =formData.get("title")as string;
+const content =formData.get("content") as string;
+const tags =formData.get("tags")as string;
+
+
+
+const blogData={
+    title,
+    content,
+    tags:tags.split(",").map((item)=>item.trim()).filter((item)=>item!== "")
+};
+
+const cookieStore=cookies()
+
+const res =await fetch(`${API_URL}/posts`,{
+method:"POST",
+headers:{
+    "Content-type":"application/json",
+    Cookie:cookieStore.toString(),
+},
+body:JSON.stringify(blogData)
+
+})
+
+
+console.log(res)
+if(res.status){
+    redirect("/dashboard/create-blog?success")
+}
+
+}
+
+
+
   return (
    <Card>
 <CardHeader>
@@ -11,12 +57,29 @@ export default function CreateBlogFormServer() {
     </CardDescription>
 </CardHeader>
 <CardContent>
-    <form>
+    <form id="blog-form" action={createBlog}>
+<FieldGroup>
+<Field>
+    <FieldLabel>Title</FieldLabel>
+    <Input id="title" type="text" name="title"/>
+  
+</Field>
+<Field>
+    <FieldLabel>Content</FieldLabel>
+    <Input id="content" type="text" name="content"/>
+  
+</Field>
+<Field>
+    <FieldLabel>Tags</FieldLabel>
+    <Input id="tags" type="text" name="tags"/>
+  
+</Field>
 
+</FieldGroup>
     </form>
 </CardContent>
 <CardFooter>
-    <Button type="submit">submit</Button>
+    <Button form="blog-form" type="submit">submit</Button>
 </CardFooter>
    </Card>
   )
